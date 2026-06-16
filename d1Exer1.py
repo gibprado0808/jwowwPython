68ed862f26424e61a8de783aad3ce1016f9c42fe
gibprado0808 <gibprado08@gmail.com>
2026-06-16 08:47:43 +0800

    create d1 exer1

---
+++ d1Exer1.py
@@ -0,0 +1,3 @@
+print("I am Gibson F. Prado")
+print("I currently work at LGU Malay as an Information Technologist.")
+print('My favorite qoute is:"In life, when things get down and don\'t look great... keep your mind straight and RUN THROUGH THE TAPE!" by Chris Webber')
